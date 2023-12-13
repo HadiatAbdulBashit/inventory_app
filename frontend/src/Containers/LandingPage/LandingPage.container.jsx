b1082ba6322f912e045c36e070f8ba4ee4679d0e
Hadiat Abdul Bashit <hadiatab16@gmail.com>
2023-12-13 08:56:57 +0700

    add landing page

--- frontend/src/Containers/LandingPage/LandingPage.container.jsx
+++ frontend/src/Containers/LandingPage/LandingPage.container.jsx
@@ -1,6 +1,31 @@
+import { Link } from 'react-router-dom';
+
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <header className="py-5">
+      <div className="container px-5 pb-5">
+        <div className="row gx-5 align-items-center">
+          <div className="col-xxl-5">
+            <div className="text-center text-xxl-start">
+              <div className="badge bg-primary text-white mb-4"><div className="text-uppercase">Design · Development · Marketing</div></div>
+              <div className="fs-3 fw-light text-muted">I can help your business to</div>
+              <h1 className="display-3 fw-bolder mb-5"><span className="text-gradient d-inline">Get faster and grow big</span></h1>
+              <div className="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xxl-start mb-3">
+                <Link className="btn btn-primary btn-lg px-5 py-3 me-sm-3 fs-6 fw-bolder" to="/login">Login</Link>
+                <Link className="btn btn-outline-dark btn-lg px-5 py-3 fs-6 fw-bolder" to="/dashboard">Dashboard</Link>
+              </div>
+            </div>
+          </div>
+          <div className="col-xxl-7">
+            <div className="d-flex justify-content-center mt-5 mt-xxl-0">
+              <div className="profile bg-primary">
+                <img className="profile-img" src="assets/profile.png" alt="..." />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
