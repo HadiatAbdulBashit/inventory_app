5c03664dfcf6be473b149d3622a92c06147d0f28
Hadiat Abdul Bashit <hadiatab16@gmail.com>
2023-12-13 08:57:12 +0700

    add navbar for template

--- frontend/src/Containers/Root/Root.container.jsx
+++ frontend/src/Containers/Root/Root.container.jsx
@@ -1,11 +1,15 @@
+import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Navbar from '../../Components/Navbar/Navbar.components';
 
 const Root = () => {
+
+  React.lazy
 
   return (
     <>
       <header>
-        dashboard
+        <Navbar />
       </header>
       <main>
         <Outlet />
