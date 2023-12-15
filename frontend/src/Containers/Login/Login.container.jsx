475dfaa412ae390ea6c74bda244f8c6f5c27b545
Hadiat Abdul Bashit <hadiatab16@gmail.com>
2023-12-15 16:30:42 +0700

    fix responsive

--- frontend/src/Containers/Login/Login.container.jsx
+++ frontend/src/Containers/Login/Login.container.jsx
@@ -4,14 +4,14 @@ const Login = () => {
 
       <div className="p-5 bg-image" style={{ background: 'linear-gradient(130deg, var(--color-one) 0%, var(--color-four) 100%)', height: '100vh', width: '100%' }}></div>
 
-      <div className="card mx-4 mx-md-5 shadow-5-strong col-5" style={{
+      <div className="card mx-4 mx-md-5 shadow-5-strong col-10 col-sm-8 col-md-6 col-lg-5 col-xl-4" style={{
         marginTop: '-80vh',
         background: 'hsla(0, 0%, 100%, 0.5)',
         backdropFilter: 'blur(20px)',
       }}>
         <div className="card-body py-5 px-md-5">
           <div className="row d-flex justify-content-center">
-            <div className="col-lg-10">
+            <div className="col-10">
               <h2 className="fw-bold mb-5">Sign up</h2>
               <form>
                 <div className="form-floating mb-3">
