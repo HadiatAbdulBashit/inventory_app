179440ac415a61a2fc8c2be9f9f5a0f9c5795f1b
Hadiat Abdul Bashit <hadiatab16@gmail.com>
2023-12-19 11:51:13 +0700

    add logout function

--- frontend/src/Components/Navbar/Navbar.components.jsx
+++ frontend/src/Components/Navbar/Navbar.components.jsx
@@ -1,7 +1,27 @@
+import { useContext } from 'react';
 import { FaBars, FaRegUser } from 'react-icons/fa6';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+import UserContext from '../../Contexts/UserContext';
 
 const Navbar = ({ setCollapsed, collapsed }) => {
+    const navigate = useNavigate();
+
+    const { setUser } = useContext(UserContext)
+
+    const handleLogout = async () => {
+        try {
+            const response = await axios.post('/api/auth/logout');
+            toast.success(response.data.message)
+            setUser({ isLoggedIn: false })
+            navigate('/')
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
+    }
+
     return (
         <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <button className="btn text-white ms-2" onClick={() => setCollapsed(!collapsed)}>
@@ -18,7 +38,7 @@ const Navbar = ({ setCollapsed, collapsed }) => {
                         </li>
                         <li><hr className="dropdown-divider" /></li>
                         <li>
-                            <Link className="dropdown-item" to={'/'}>Logout</Link>
+                            <Link className="dropdown-item" onClick={handleLogout}>Logout</Link>
                         </li>
                     </ul>
                 </li>
