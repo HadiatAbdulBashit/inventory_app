cc00f639b06ef5fe8540139236bcd36c4a57a1f4
Hadiat Abdul Bashit <hadiatab16@gmail.com>
2023-12-22 11:37:21 +0700

    call model and add relation between table

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,8 +1,13 @@
 const dbConfig = require("../config/db.config.js");
 const Sequelize = require("sequelize");
 const User  = require("./user.model.js");
-const Transaction = require("./transactionIn.model.js")
-const Goods = require("./goods.model.js")
+const Purchase = require("./purchase.model.js")
+const PurchaseDetail = require("./purchaseDetail.model.js")
+const Sale = require("./sale.model.js")
+const SaleDetail = require("./saleDetail.model.js")
+const Item = require("./item.model.js")
+const ItemDetail = require("./itemDetail.model.js")
+const ItemPrice = require("./itemPrice.model.js")
 
 const table = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
@@ -21,8 +26,69 @@ const db = {};
 
 db.sequelize = table;
 
-db.users = User(table);
-db.transaction = Transaction(table);
-db.goods = Goods(table);
+db.user = User(table);
+db.purchase = Purchase(table);
+db.purchaseDetail = PurchaseDetail(table);
+db.sale = Sale(table);
+db.saleDetail = SaleDetail(table);
+db.item = Item(table);
+db.itemPrice = ItemPrice(table);
+db.itemDetail = ItemDetail(table);
+
+// User and Purchase Relationship
+db.user.hasMany(db.purchase, { as: "purchase" });
+db.purchase.belongsTo(db.user, {
+  foreignKey: "userId",
+  as: "user",
+});
+
+// User and Sale Relationship
+db.user.hasMany(db.sale, { as: "sale" });
+db.sale.belongsTo(db.user, {
+  foreignKey: "userId",
+  as: "user",
+});
+
+// Item and ItemDetail Relationship
+db.item.hasMany(db.itemDetail, { as: "itemDetail" });
+db.itemDetail.belongsTo(db.item, {
+  foreignKey: "itemId",
+  as: "item",
+});
+
+// Item and ItemPrice Relationship
+db.item.hasMany(db.itemPrice, { as: "itemPrice" });
+db.itemPrice.belongsTo(db.item, {
+  foreignKey: "itemId",
+  as: "item",
+});
+
+// Purchase and PurchaseDetail Relationship
+db.purchase.hasMany(db.purchaseDetail, { as: "purchaseDetail" });
+db.purchaseDetail.belongsTo(db.purchase, {
+  foreignKey: "purchaseId",
+  as: "purchase",
+});
+
+// Item and PurchaseDetail Relationship
+db.item.hasMany(db.purchaseDetail, { as: "purchaseDetail" });
+db.purchaseDetail.belongsTo(db.item, {
+  foreignKey: "itemId",
+  as: "item",
+});
+
+// Purchase and SaleDetail Relationship
+db.sale.hasMany(db.saleDetail, { as: "saleDetail" });
+db.saleDetail.belongsTo(db.sale, {
+  foreignKey: "saleId",
+  as: "sale",
+});
+
+// Item and SaleDetail Relationship
+db.item.hasMany(db.saleDetail, { as: "saleDetail" });
+db.saleDetail.belongsTo(db.item, {
+  foreignKey: "itemId",
+  as: "item",
+});
 
 module.exports = db;
