68e25a82cffd55717a7f564bb3d974a1a5458afc
Hadiat Abdul Bashit <hadiatab16@gmail.com>
2023-12-27 17:04:48 +0700

    add item detail routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,17 +11,24 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 
 import Root from "./Containers/Root/Root.container";
 import LandingPage from './Containers/LandingPage/LandingPage.container';
+import Acount from './Containers/Acount/Acount.container';
 import Login from './Containers/Login/Login.container';
 import ProtectedRoute from './Containers/ProtectedRoute/ProtectedRoute.container';
 import Dashboard from './Containers/Dashboard/Dashboard.container';
+
 import Purchase from './Containers/Purchase/Purchase.container';
 import Sale from './Containers/Sale/Sale.container';
 import User from './Containers/User/User.container';
-import Acount from './Containers/Acount/Acount.container';
+
+// Item page
 import AddItem from './Containers/Item/AddItem.container';
 import EditItem from './Containers/Item/EditItem.container';
 import Items from './Containers/Item/Items.container';
 import Item from './Containers/Item/Item.container';
+
+// Item Detail page
+import AddItemDetail from './Containers/Item/AddItemDetail.container';
+import EditItemDetail from './Containers/Item/EditItemDetail.container';
 
 import UserContext from './Contexts/UserContext';
 
@@ -57,6 +64,10 @@ function App() {
               <Route path='add' element={<AddItem />} />
               <Route path=':id' element={<Item />} />
               <Route path=':id/edit' element={<EditItem />} />
+            </Route>
+            <Route path='item-detail'>
+              <Route path='add/:id' element={<AddItemDetail />} />
+              <Route path=':id/edit' element={<EditItemDetail />} />
             </Route>
             <Route path='purchase' element={<Purchase />} />
             <Route path='sale' element={<Sale />} />
