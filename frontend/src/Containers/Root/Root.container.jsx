4300800cc42e3a094df5a172fdadba9e7ae2b43c
Hadiat Abdul Bashit <hadiatab16@gmail.com>
2023-12-28 19:26:22 +0700

    delete navbar

--- frontend/src/Containers/Root/Root.container.jsx
+++ frontend/src/Containers/Root/Root.container.jsx
@@ -1,20 +1,15 @@
-import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import Navbar from '../../Components/Navbar/Navbar.component';
 import Sidebar from '../../Components/Sidebar/Sidebar.component';
 
 import style from './Root.module.css'
 
 const Root = () => {
-  const [collapsed, setCollapsed] = useState(false);
-
   return (
     <div className={style.wrapper}>
       <div style={{ display: 'flex', minHeight: '100vh' }}>
-        <Sidebar collapsed={collapsed} />
+        <Sidebar/>
       </div>
       <main className={style.main}>
-        <Navbar setCollapsed={setCollapsed} collapsed={collapsed} />
         <Outlet />
       </main>
     </div>
