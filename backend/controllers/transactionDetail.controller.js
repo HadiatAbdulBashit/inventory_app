20ba946487244f18695018adeb4888245d4b7c2f
Hadiat Abdul Bashit <hadiatab16@gmail.com>
2024-01-03 14:08:51 +0700

    add function to update total price in transaction

--- backend/controllers/transactionDetail.controller.js
+++ backend/controllers/transactionDetail.controller.js
@@ -4,29 +4,70 @@ const { Sequelize } = require('sequelize');
 const db = require("../models");
 
 const TransactionDetail = db.transactionDetail
+const Transaction = db.transaction;
 const ItemDetail = db.itemDetail;
 const Item = db.item;
 const Op = Sequelize.Op;
 
 // Create and Save a new Transaction Detail
-exports.create = async(req, res) => {
+exports.create = async (req, res) => {
     // Validate request
     if (!req.body.totalItem || req.body.totalItem === 0) {
         res.status(400).send({
-            message: "Total item can not be empty!"
+            msg: "Total item can not be empty!"
         });
         return;
     }
 
-    var condition = { 
-        itemDetailId: { [Op.iLike]: req.body.itemDetailId }, 
+    let condition = {
+        itemDetailId: { [Op.iLike]: req.body.itemDetailId },
         transactionId: { [Op.iLike]: req.body.transactionId }
     };
 
+    let price = null;
+    let oldPrice = null;
+    let updateStock = null;
 
-    TransactionDetail.findAll({ 
+    ItemDetail.findOne({
+        where: { id: req.body.itemDetailId }
+    })
+        .then(data => {
+            if (data) {
+                price = data.price * req.body.totalItem
+                updateStock = data.stock - req.body.totalItem
+            } else {
+                res.status(404).send({
+                    msg: `Cannot find Item Detail with id=${id}.`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                msg: "Error retrieving Item Detail with id=" + id
+            });
+        });
+
+    Transaction.findOne({
+        where: { id: req.body.transactionId }
+    })
+        .then(data => {
+            if (data) {
+                oldPrice = data.totalPrice
+            } else {
+                res.status(404).send({
+                    msg: `Cannot find Item Detail with id=${id}.`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                msg: "Error retrieving Item Detail with id=" + id
+            });
+        });
+
+    TransactionDetail.findAll({
         where: condition,
-     })
+    })
         .then(data => {
             if (data.length > 0) {
                 res.status(401).send({
@@ -36,6 +77,21 @@ exports.create = async(req, res) => {
                 // Save Transaction Detail in the database
                 TransactionDetail.create(req.body)
                     .then(data => {
+                        Transaction.update({ totalPrice: price + oldPrice }, {
+                            where: { id: req.body.transactionId }
+                        })
+                            .then(num => {
+                                if (num < 1) {
+                                    res.send({
+                                        msg: `Cannot update Transaction with id = ${req.body.transactionId}. Maybe Transaction was not found or request is empty!`
+                                    });
+                                }
+                            })
+                            .catch(err => {
+                                res.status(500).send({
+                                    msg: "Error updating Transaction with id = " + req.body.transactionId
+                                });
+                            });
                         res.send(data);
                     })
                     .catch(err => {
@@ -48,7 +104,7 @@ exports.create = async(req, res) => {
         })
         .catch(err => {
             res.status(500).send({
-                message:
+                msg:
                     err.message || "Some error occurred while retrieving transaction detail."
             });
         });
@@ -60,7 +116,7 @@ exports.create = async(req, res) => {
 exports.findAll = (req, res) => {
     var condition = req.query.transactionId ? { transactionId: { [Op.iLike]: `%${req.query.transactionId}%` } } : null;
 
-    TransactionDetail.findAll({ 
+    TransactionDetail.findAll({
         where: condition,
         order: [['createdAt', 'desc']],
         include: [{
@@ -73,13 +129,13 @@ exports.findAll = (req, res) => {
                 as: 'item', // Specify the alias for the association
             }]
         }]
-     })
+    })
         .then(data => {
             res.send(data);
         })
         .catch(err => {
             res.status(500).send({
-                message:
+                msg:
                     err.message || "Some error occurred while retrieving transaction detail."
             });
         });
@@ -109,13 +165,13 @@ exports.findOne = (req, res) => {
                 res.send(data);
             } else {
                 res.status(404).send({
-                    message: `Cannot find Transaction Detail with id=${id}.`
+                    msg: `Cannot find Transaction Detail with id=${id}.`
                 });
             }
         })
         .catch(err => {
             res.status(500).send({
-                message: "Error retrieving Transaction Detail with id=" + id
+                msg: "Error retrieving Transaction Detail with id=" + id
             });
         });
 };
@@ -130,17 +186,17 @@ exports.update = (req, res) => {
         .then(num => {
             if (num == 1) {
                 res.send({
-                    message: "Transaction Detail was updated successfully."
+                    msg: "Transaction Detail was updated successfully."
                 });
             } else {
                 res.send({
-                    message: `Cannot update Transaction Detail with id=${id}. Maybe Transaction Detail was not found or req.body is empty!`
+                    msg: `Cannot update Transaction Detail with id=${id}. Maybe Transaction Detail was not found or req.body is empty!`
                 });
             }
         })
         .catch(err => {
             res.status(500).send({
-                message: "Error updating Transaction Detail with id=" + id
+                msg: "Error updating Transaction Detail with id=" + id
             });
         });
 };
@@ -155,17 +211,17 @@ exports.delete = (req, res) => {
         .then(num => {
             if (num == 1) {
                 res.send({
-                    message: "TransactionDetail was deleted successfully!"
+                    msg: "TransactionDetail was deleted successfully!"
                 });
             } else {
                 res.send({
-                    message: `Cannot delete Transaction Detail with id=${id}. Maybe Transaction Detail was not found!`
+                    msg: `Cannot delete Transaction Detail with id=${id}. Maybe Transaction Detail was not found!`
                 });
             }
         })
         .catch(err => {
             res.status(500).send({
-                message: "Could not delete Transaction Detail with id=" + id
+                msg: "Could not delete Transaction Detail with id=" + id
             });
         });
 };
