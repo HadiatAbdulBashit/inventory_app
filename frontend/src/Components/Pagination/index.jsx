850b92035bfb8e339096afd6784b7fad5fb5c11d
Hadiat Abdul Bashit <hadiatab16@gmail.com>
2024-01-05 17:42:41 +0700

    change style and function pagination

--- frontend/src/Components/Pagination/index.jsx
+++ frontend/src/Components/Pagination/index.jsx
@@ -1,29 +1,55 @@
-import styles from "./styles.module.css";
-
 const Pagination = ({ page, total, limit, setPage }) => {
 	const totalPages = Math.ceil(total / limit);
 
 	const onClick = (newPage) => {
-		setPage(newPage + 1);
+		setPage(newPage);
+	};
+
+	const handlePrevious = () => {
+		if (page === 1) {
+			return
+		}
+		setPage(page - 1)
+	}
+
+	const handleNext = () => {
+		if (page === total) {
+			return
+		}
+		setPage(page + 1)
+	}
+
+	const renderPageNumbers = () => {
+		const visiblePages = [];
+		const minPage = Math.max(1, page - 1);
+		const maxPage = Math.min(totalPages, page + 1);
+
+		for (let i = minPage; i <= maxPage; i++) {
+			visiblePages.push(
+				<li className="page-item" key={i}>
+					<button
+						onClick={() => onClick(i)}
+						className={page === i ? `page-link active` : 'page-link'}
+					>
+						{i}
+					</button>
+				</li>
+			);
+		}
+
+		return visiblePages;
 	};
 
 	return (
-		<div className={styles.container}>
-			{totalPages > 0 &&
-				[...Array(totalPages)].map((val, index) => (
-					<button
-						onClick={() => onClick(index)}
-						className={
-							page === index + 1
-								? `${styles.page_btn} ${styles.active}`
-								: styles.page_btn
-						}
-						key={index}
-					>
-						{index + 1}
-					</button>
-				))}
-		</div>
+		<ul className="pagination justify-content-end">
+			<li className={"page-item " + (page === 1 ? 'disabled' : null)}>
+				<button className="page-link" onClick={() => handlePrevious()}>&laquo;</button>
+			</li>
+			{renderPageNumbers()}
+			<li className="page-item">
+				<button className={"page-link " + (page === totalPages ? 'disabled' : null)} onClick={() => handleNext()}>&raquo;</button>
+			</li>
+		</ul>
 	);
 };
 
