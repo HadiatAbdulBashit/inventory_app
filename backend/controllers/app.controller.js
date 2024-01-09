a0db709e5e1a9843edc1bbfa2648266b4629e693
Hadiat Abdul Bashit <hadiatab16@gmail.com>
2024-01-09 11:22:08 +0700

    add, delete comment and add status condition

--- backend/controllers/app.controller.js
+++ backend/controllers/app.controller.js
@@ -11,24 +11,24 @@ const Op = Sequelize.Op;
 // Retrieve necersery data from the database.
 exports.dashboard = async (req, res) => {
     try {
-        // Total Purchase This Month
+        // Total Price Purchase This Month
         const totalPurchase = await Transaction.sum('totalPrice', {
             where: {
-                type: 'In', // Assuming 'In' represents a purchase
-                status: 'Success', // You might want to adjust the status based on your logic
+                type: 'In',
+                status: ['Success', 'Success with Return'],
                 createdAt: {
-                    [Op.gte]: new Date(new Date() - 30 * 24 * 60 * 60 * 1000), // Filter for the last 30 days
+                    [Op.gte]: new Date(new Date() - 30 * 24 * 60 * 60 * 1000),
                 },
             }
         });
 
-        // Total Sale This Month
+        // Total Purchase Sale This Month
         const totalSale = await Transaction.sum('totalPrice', {
             where: {
-                type: 'Out', // Assuming 'Out' represents a sale
-                status: 'Success', // You might want to adjust the status based on your logic
+                type: 'Out',
+                status: ['Success', 'Success with Return'],
                 createdAt: {
-                    [Op.gte]: new Date(new Date() - 30 * 24 * 60 * 60 * 1000), // Filter for the last 30 days
+                    [Op.gte]: new Date(new Date() - 30 * 24 * 60 * 60 * 1000),
                 },
             }
         });
@@ -36,9 +36,9 @@ exports.dashboard = async (req, res) => {
         // Total Transaction This Month
         const totalTransaction = await Transaction.count({
             where: {
-                status: 'Success', // You might want to adjust the status based on your logic
+                status: ['Success', 'Success with Return'],
                 createdAt: {
-                    [Op.gte]: new Date(new Date() - 30 * 24 * 60 * 60 * 1000), // Filter for the last 30 days
+                    [Op.gte]: new Date(new Date() - 30 * 24 * 60 * 60 * 1000),
                 },
             }
         });
@@ -46,6 +46,7 @@ exports.dashboard = async (req, res) => {
         // Item in Warehouse
         const itemsInWarehouse = await Item.count();
 
+        // Create name of past 7 month from now
         const currentMonth = new Date().getMonth();
         const barChartMonths = Array.from({ length: 7 }, (_, i) => {
             const monthIndex = (currentMonth - i + 12) % 12;
@@ -59,8 +60,8 @@ exports.dashboard = async (req, res) => {
         for (const month of barChartMonths) {
             const saleData = await Transaction.sum('totalPrice', {
                 where: {
-                    type: 'Out', // Assuming 'Out' represents a sale
-                    status: 'Success',
+                    type: 'Out',
+                    status: ['Success', 'Success with Return'],
                     createdAt: {
                         [Op.between]: [
                             new Date(new Date().getFullYear(), barChartMonths.indexOf(month), 1),
@@ -72,8 +73,8 @@ exports.dashboard = async (req, res) => {
 
             const purchaseData = await Transaction.sum('totalPrice', {
                 where: {
-                    type: 'In', // Assuming 'In' represents a purchase
-                    status: 'Success',
+                    type: 'In',
+                    status: ['Success', 'Success with Return'],
                     createdAt: {
                         [Op.between]: [
                             new Date(new Date().getFullYear(), barChartMonths.indexOf(month), 1),
@@ -87,44 +88,51 @@ exports.dashboard = async (req, res) => {
             barChartPurchaseData.push(purchaseData || 0);
         }
 
+        // Get total purchase this month
         const totalPurchaseThisMonth = await Transaction.count({
             where: {
                 type: 'In',
-                status: 'Success',
+                status: ['Success', 'Success with Return'],
                 createdAt: {
-                    [Op.gte]: new Date(new Date() - 30 * 24 * 60 * 60 * 1000), // Filter for the last 30 days
+                    [Op.gte]: new Date(new Date() - 30 * 24 * 60 * 60 * 1000),
                 },
             }
         });
-        
+
+        // Get total sale this month
         const totalSaleThisMonth = await Transaction.count({
             where: {
                 type: 'Out',
-                status: 'Success',
+                status: ['Success', 'Success with Return'],
                 createdAt: {
-                    [Op.gte]: new Date(new Date() - 30 * 24 * 60 * 60 * 1000), // Filter for the last 30 days
+                    [Op.gte]: new Date(new Date() - 30 * 24 * 60 * 60 * 1000),
                 },
             }
         });
-        
+
+        // Get 5 last purchase
         const lastPurchase = await Transaction.findAll({
             where: {
                 type: 'In',
+                status: ['Success', 'Success with Return', 'Canceled'],
             },
-            attributes: ['secondParty', 'status'],
+            attributes: ['secondParty', 'status', 'updatedAt'],
             limit: 5,
             order: [['createdAt', 'desc']],
         });
 
+        // Get last sale
         const lastSale = await Transaction.findAll({
             where: {
                 type: 'Out',
+                status: ['Success', 'Success with Return', 'Canceled'],
             },
-            attributes: ['secondParty', 'status'],
+            attributes: ['secondParty', 'status', 'updatedAt'],
             limit: 5,
             order: [['createdAt', 'desc']],
         });
 
+        // Get last updated item
         const newItem = await ItemDetail.findAll({
             attributes: ['unit', 'stock'],
             limit: 5,
@@ -164,22 +172,23 @@ exports.appLog = (req, res) => {
     const limit = parseInt(req.query.limit) || 10;
     let sort = req.query.sort || "createdAt";
     let total = null
-    let filterMethod = req.query.method || "All";
     req.query.sort ? (sort = req.query.sort.split(",")) : (sort = [sort]);
 
+    // filter search base on message
+    var condition = req.query.search ? { message: { [Op.iLike]: `%${req.query.search}%` } } : null;
+
+    // filter base on methode request
+    let filterMethod = req.query.method || "All";
     const methodeOption = [
         "GET",
         "POST",
         "PUT",
         "DELETE",
     ];
-    
     filterMethod === "All" ? filterMethod = [...methodeOption] : filterMethod = req.query.method.split(",");
-
-    var condition = req.query.search ? { message: { [Op.iLike]: `%${req.query.search}%` } } : null;
-
     condition = req.query.method && condition ? { ...condition, method: { [Op.or]: filterMethod } } : condition ? condition : req.query.method ? { method: { [Op.or]: filterMethod } } : null;
 
+    // filter date range base on transaction created
     if (req.query.startDate && req.query.endDate) {
         let startDate = new Date(req.query.startDate);
         startDate = startDate.toISOString().split('T')[0];
@@ -188,14 +197,14 @@ exports.appLog = (req, res) => {
         endDate = endDate.setDate(endDate.getDate() + 1)
         let newEndDate = new Date(endDate)
         newEndDate = newEndDate.toISOString().split('T')[0];
-        
+
         condition = {
             ...condition,
             createdAt: {
                 [Op.between]: [startDate, newEndDate],
             },
         };
-    } else if (req.query.month && req.query.year) {
+    } else if (req.query.month && req.query.year) { // filter date by month base on transaction created
         const firstDayOfMonth = new Date(req.query.year, req.query.month - 1, 1);
         const lastDayOfMonth = new Date(req.query.year, req.query.month, 0);
 
@@ -207,7 +216,8 @@ exports.appLog = (req, res) => {
             },
         };
     }
-    
+
+    // count data form app log table with in condition
     AppLog.count({
         where: condition
     })
@@ -219,6 +229,7 @@ exports.appLog = (req, res) => {
             });
         });
 
+    // get data form app log table with in condition, limit, offset and order
     AppLog.findAll({
         where: condition,
         offset: page * limit,
@@ -226,6 +237,7 @@ exports.appLog = (req, res) => {
         order: [sort]
     })
         .then(logs => {
+            // send response
             res.send({
                 page,
                 limit,
